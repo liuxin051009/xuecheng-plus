dd6d93ff4fe61cd42ee95ded2df4a36250610670
liuxin051009 <279291717@qq.com>
2023-04-29 08:41:13 +0800

    逆向工程

---
+++ xuecheng-plus-content/xuecheng-plus-content-model/src/main/java/com/xuecheng/model/po/CourseMarket.java
@@ -0,0 +1,63 @@
+package com.xuecheng.model.po;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 课程营销信息
+ * </p>
+ *
+ * @author meteor
+ * @since 2023-04-28
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class CourseMarket implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 主键，课程id
+     */
+    private Long id;
+
+    /**
+     * 收费规则，对应数据字典
+     */
+    private String charge;
+
+    /**
+     * 现价
+     */
+    private Float price;
+
+    /**
+     * 原价
+     */
+    private Float originalPrice;
+
+    /**
+     * 咨询qq
+     */
+    private String qq;
+
+    /**
+     * 微信
+     */
+    private String wechat;
+
+    /**
+     * 电话
+     */
+    private String phone;
+
+    /**
+     * 有效期天数
+     */
+    private Integer validDays;
+
+
+}
---
+++ xuecheng-plus-content/xuecheng-plus-content-model/src/main/java/com/xuecheng/model/po/TeachplanMedia.java
@@ -0,0 +1,68 @@
+package com.xuecheng.model.po;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author meteor
+ * @since 2023-04-28
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class TeachplanMedia implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 主键
+     */
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 媒资文件id
+     */
+    private String mediaId;
+
+    /**
+     * 课程计划标识
+     */
+    private Long teachplanId;
+
+    /**
+     * 课程标识
+     */
+    private Long courseId;
+
+    /**
+     * 媒资文件原始名称
+     */
+    @TableField("media_fileName")
+    private String mediaFilename;
+
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createDate;
+
+    /**
+     * 创建人
+     */
+    private String createPeople;
+
+    /**
+     * 修改人
+     */
+    private String changePeople;
+
+
+}
